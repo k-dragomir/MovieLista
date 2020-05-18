3e154de0374f31b18e27c38733b8a8c548f22887
Ekaterina Dragomirova <k.dragomirova@gmail.com>
2020-05-18 21:29:21 +0300

    Конечный набор представлений

--- views.sql
+++ views.sql
@@ -1,183 +1,446 @@
 USE movielista;
 
--- ----------------------------------- FUNCTIONS
+-- ---------------------------------------------------------
+-- ----------------------- FUNCTIONS -----------------------
+-- ---------------------------------------------------------
 
--- Функция расчета средневзвешенного рейтинга
-DROP FUNCTION IF EXISTS wavg_rating;
+
+-- ----------------------------------- GENRE RELEVANCY COUNT function
+DROP FUNCTION IF EXISTS g_relevancy;
 DELIMITER //
-CREATE FUNCTION wavg_rating(t_id INT UNSIGNED)
-	RETURNS FLOAT UNSIGNED DETERMINISTIC
-BEGIN
-	DECLARE rate1 INT UNSIGNED;
-	DECLARE rate2 INT UNSIGNED;
-	DECLARE rate3 INT UNSIGNED;
-	DECLARE rate4 INT UNSIGNED;
-	DECLARE rate5 INT UNSIGNED;
-	DECLARE rate6 INT UNSIGNED;
-	DECLARE rate7 INT UNSIGNED;
-	DECLARE rate8 INT UNSIGNED;
-	DECLARE rate9 INT UNSIGNED;
-	DECLARE rate10 INT UNSIGNED;
-
-	DECLARE num1 INT UNSIGNED;
-	DECLARE num2 INT UNSIGNED;
-	DECLARE num3 INT UNSIGNED;
-	DECLARE num4 INT UNSIGNED;
-	DECLARE num5 INT UNSIGNED;
-	DECLARE num6 INT UNSIGNED;
-	DECLARE num7 INT UNSIGNED;
-	DECLARE num8 INT UNSIGNED;
-	DECLARE num9 INT UNSIGNED;
-	DECLARE num10 INT UNSIGNED;
-
-	SET rate1 = 1;
-	SET rate2 = 2;
-	SET rate3 = 3;
-	SET rate4 = 4;
-	SET rate5 = 5;
-	SET rate6 = 6;
-	SET rate7 = 7;
-	SET rate8 = 8;
-	SET rate9 = 9;
-	SET rate10 = 10;
-
-	SET num1 = (SELECT count(*) FROM rating WHERE rating = rate1 AND title_id = t_id);
-	SET num2 = (SELECT count(*) FROM rating WHERE rating = rate2 AND title_id = t_id);
-	SET num3 = (SELECT count(*) FROM rating WHERE rating = rate3 AND title_id = t_id);
-	SET num4 = (SELECT count(*) FROM rating WHERE rating = rate4 AND title_id = t_id);
-	SET num5 = (SELECT count(*) FROM rating WHERE rating = rate5 AND title_id = t_id);
-	SET num6 = (SELECT count(*) FROM rating WHERE rating = rate6 AND title_id = t_id);
-	SET num7 = (SELECT count(*) FROM rating WHERE rating = rate7 AND title_id = t_id);
-	SET num8 = (SELECT count(*) FROM rating WHERE rating = rate8 AND title_id = t_id);
-	SET num9 = (SELECT count(*) FROM rating WHERE rating = rate9 AND title_id = t_id);
-	SET num10 = (SELECT count(*) FROM rating WHERE rating = rate10 AND title_id = t_id);
-
-	RETURN (rate1 * num1 + rate2 * num2 + rate3 * num3 + rate4 * num4 + rate5 * num5 +
-			rate6 * num6 + rate7 * num7 + rate8 * num8 + rate9 * num9 + rate10 * num10)
-		/ (num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10);
-END;
-//
-DELIMITER ;
-
--- Функция расчета соответствия ключевого слова фильму (по пользовательским голосованиям)
-DROP FUNCTION IF EXISTS keywords;
-DELIMITER //
-CREATE FUNCTION keywords(t_id INT, k_id INT)
+CREATE FUNCTION g_relevancy(t_id INT UNSIGNED, g_id INT UNSIGNED)
 	RETURNS INT DETERMINISTIC
 BEGIN
-	RETURN (SELECT 2 * plus_tag.c - all_tags.c
+	RETURN (SELECT likes.c - dislikes.c
 			  FROM (
 					   SELECT count(vote) AS c
-						 FROM title_keyword
-						WHERE vote = 1 AND title_id = t_id AND keyword_id = k_id
-				   ) AS plus_tag
-					   JOIN (SELECT count(vote) AS c FROM title_keyword WHERE title_id = t_id AND keyword_id = k_id
-							) AS all_tags
-		   );
-END;
-//
-DELIMITER ;
-
--- Функция расчета соответствия жанра фильму (по пользовательским голосованиям)
-DROP FUNCTION IF EXISTS genres;
-DELIMITER //
-CREATE FUNCTION genres(t_id INT, g_id INT)
-	RETURNS INT DETERMINISTIC
-BEGIN
-	RETURN (SELECT 2 * plus_tag.c - all_tags.c
-			  FROM (
-					   SELECT count(vote) AS c
-						 FROM title_genre
+						 FROM votes_on_genre
 						WHERE vote = 1 AND title_id = t_id AND genre_id = g_id
-				   ) AS plus_tag
-					   JOIN (SELECT count(vote) AS c FROM title_genre WHERE title_id = t_id AND genre_id = g_id
-							) AS all_tags
+				   ) AS likes
+					   JOIN (
+				  SELECT count(vote) AS c
+					FROM votes_on_genre
+				   WHERE vote = 0 AND title_id = t_id AND genre_id = g_id
+							) AS dislikes
 		   );
 END;
 //
 DELIMITER ;
+-- DROP FUNCTION IF EXISTS g_relevancy;
 
 
--- ----------------------------------- RATINGS view
+-- ----------------------------------- KEYWORD RELEVANCY COUNT function
+DROP FUNCTION IF EXISTS k_relevancy;
+DELIMITER //
+CREATE FUNCTION k_relevancy(t_id INT UNSIGNED, k_id INT UNSIGNED)
+	RETURNS INT DETERMINISTIC
+BEGIN
+	RETURN (SELECT likes.c - dislikes.c
+			  FROM (
+					   SELECT count(vote) AS c
+						 FROM votes_on_keywords
+						WHERE vote = 1 AND title_id = t_id AND keyword_id = k_id
+				   ) AS likes
+					   JOIN (
+				  SELECT count(vote) AS c
+					FROM votes_on_keywords
+				   WHERE vote = 0 AND title_id = t_id AND keyword_id = k_id
+							) AS dislikes
+		   );
+END;
+//
+DELIMITER ;
+-- DROP FUNCTION IF EXISTS k_relevancy;
 
-CREATE OR REPLACE ALGORITHM = TEMPTABLE VIEW t_ratings AS
-	SELECT t.id,
-		   t.title AS title,
-		   r.avg_rating,
-		   r.wavg_rating,
-		   r.count
+
+-- ----------------------------------- REVIEW RATING function
+DROP FUNCTION IF EXISTS review_rate;
+DELIMITER //
+CREATE FUNCTION review_rate(r_id INT UNSIGNED)
+	RETURNS INT DETERMINISTIC
+BEGIN
+	RETURN (SELECT likes.c - dislikes.c
+			  FROM (
+					   SELECT count(vote) AS c
+						 FROM votes_on_reviews
+						WHERE vote = 1 AND review_id = r_id
+				   ) AS likes
+					   JOIN (
+				  SELECT count(vote) AS c
+					FROM votes_on_reviews
+				   WHERE vote = 0 AND review_id = r_id
+							) AS dislikes
+		   );
+END;
+//
+DELIMITER ;
+-- DROP FUNCTION IF EXISTS review_rate;
+
+
+-- ---------------------------------------------------------
+-- ----------------------- BASIC VIEWS ---------------------
+-- ---------------------------------------------------------
+
+-- ----------------------------------- COUNTRIES GENERAL INFO view
+CREATE OR REPLACE VIEW countries_info AS
+	SELECT c.id AS c_id,
+		   c.country,
+		   tc.c1 AS all_titles,
+		   up.c2 AS all_users,
+		   cr.c3 AS all_creators
+	  FROM countries c
+			   LEFT JOIN (SELECT count(title_id) AS c1,
+								 country_id
+							FROM title_country
+						   GROUP BY country_id
+						 ) AS tc ON c.id = tc.country_id
+			   LEFT JOIN (SELECT count(user_id) AS c2,
+								 country_id
+							FROM user_profiles
+						   GROUP BY country_id
+						 ) AS up ON c.id = up.country_id
+			   LEFT JOIN (SELECT count(id) AS c3,
+								 country_id
+							FROM creators
+						   GROUP BY country_id
+						 ) AS cr ON c.id = cr.country_id
+	 GROUP BY
+		 c.country
+	 ORDER BY
+		 c.country;
+-- DROP VIEW IF EXISTS countries_info;
+
+-- ----------------------------------- TITLES & COUNTRIES view
+
+CREATE OR REPLACE VIEW titles_and_countries AS
+	SELECT t.id AS t_id,
+		   t.title,
+		   c.id AS c_id,
+		   c.country
+	  FROM titles t
+			   JOIN title_country tc ON t.id = tc.title_id
+			   JOIN countries c ON tc.country_id = c.id
+	 ORDER BY
+		 t.id;
+
+
+-- ----------------------------------- TITLES & PRODUCTION COMPANIES view
+CREATE OR REPLACE VIEW titles_and_companies AS
+	SELECT t.id AS t_id,
+		   t.title,
+		   c.id AS c_id,
+		   c.company
+	  FROM titles t
+			   JOIN title_company tc ON t.id = tc.title_id
+			   JOIN companies c ON tc.company_id = c.id
+	 ORDER BY
+		 t.id;
+-- DROP VIEW IF EXISTS titles_and_companies;
+
+
+-- ----------------------------------- TITLES & CREATORS view
+CREATE OR REPLACE VIEW titles_and_cast AS
+	SELECT t.id AS t_id,
+		   t.title,
+		   r.id AS r_id,
+		   r.role,
+		   c.id AS c_id,
+		   concat_ws(' ', c.first_name, c.last_name) AS name,
+		   ti.release_date
+	  FROM creators c
+			   INNER JOIN cast_and_crew cac ON c.id = cac.creator_id
+			   INNER JOIN titles t ON cac.title_id = t.id
+			   INNER JOIN roles r ON cac.role_id = r.id
+			   INNER JOIN title_info ti ON t.id = ti.title_id
+	 ORDER BY
+		 t.id;
+-- DROP VIEW IF EXISTS titles_and_cast;
+
+
+-- ---------------------------------------------------------
+-- ----------------------- VIEWS WITH VOTES ----------------
+-- ---------------------------------------------------------
+
+
+-- ----------------------------------- GENRES GENERAL INFO view
+CREATE OR REPLACE VIEW genres_info AS
+	SELECT g.id AS g_id,
+		   g.genre,
+		   fg.c AS followers,
+		   vog.c AS all_titles,
+		   vog_r.c AS relevant_titles
+	  FROM genres g
+			   LEFT JOIN (SELECT count(user_id) AS c,
+								 genre_id
+							FROM follow_genre
+						   GROUP BY genre_id
+						 ) AS fg ON g.id = fg.genre_id
+			   LEFT JOIN (SELECT count(title_id) AS c, -- All titles
+								 genre_id
+							FROM votes_on_genre
+						   GROUP BY genre_id
+						 ) AS vog ON g.id = vog.genre_id
+			   LEFT JOIN (SELECT count(title_id) AS c,
+								 genre_id
+							FROM votes_on_genre
+						   WHERE g_relevancy(title_id, genre_id) > 0
+						   GROUP BY genre_id
+						 ) AS vog_r ON g.id = vog_r.genre_id
+	 ORDER BY
+		 g.genre;
+-- DROP VIEW IF EXISTS genres_info;
+
+
+-- ----------------------------------- TITLES & THEIR GENRES view
+CREATE OR REPLACE VIEW titles_and_genres AS
+	SELECT t.id AS t_id,
+		   t.title,
+		   g.id AS g_id,
+		   g.genre,
+		   g_relevancy(t.id, g.id) AS relevancy
+	  FROM titles t
+			   LEFT JOIN votes_on_genre vog ON t.id = vog.title_id
+			   LEFT JOIN genres g ON vog.genre_id = g.id
+	 GROUP BY
+		 t.id, g.genre
+	 ORDER BY
+		 t.id, relevancy DESC;
+-- DROP VIEW IF EXISTS titles_and_genres;
+
+
+-- ----------------------------------- KEYWORDS GENERAL INFO view
+CREATE OR REPLACE VIEW keywords_info AS
+	SELECT ak.id AS k_id,
+		   ak.keyword,
+		   fk.c AS followers,
+		   vok.c AS all_titles,
+		   vok_r.c AS relevant_titles
+	  FROM all_keywords ak
+			   LEFT JOIN (SELECT count(user_id) AS c,
+								 keyword_id
+							FROM follow_keyword
+						   GROUP BY keyword_id
+						 ) AS fk ON ak.id = fk.keyword_id
+			   LEFT JOIN (SELECT count(title_id) AS c, -- All titles
+								 keyword_id
+							FROM votes_on_keywords
+						   GROUP BY keyword_id
+						 ) AS vok ON ak.id = vok.keyword_id
+			   LEFT JOIN (SELECT count(title_id) AS c, -- Relevant titles
+								 keyword_id
+							FROM votes_on_keywords
+						   WHERE k_relevancy(title_id, keyword_id) > 0
+						   GROUP BY keyword_id
+						 ) AS vok_r ON ak.id = vok_r.keyword_id
+	 ORDER BY
+		 ak.id;
+-- DROP VIEW IF EXISTS keywords_info;
+
+
+-- ----------------------------------- TITLES & KEYWORDS view
+CREATE OR REPLACE VIEW titles_and_keywords AS
+	SELECT t.id AS t_id,
+		   t.title,
+		   ak.id AS k_id,
+		   ak.keyword,
+		   k_relevancy(t.id, ak.id) AS relevancy
+	  FROM titles t
+			   LEFT JOIN votes_on_keywords vok ON t.id = vok.title_id
+			   LEFT JOIN all_keywords ak ON vok.keyword_id = ak.id
+	 GROUP BY
+		 t.id, ak.keyword
+	 ORDER BY
+		 t.id, relevancy DESC;
+-- DROP VIEW IF EXISTS titles_and_genres;
+
+
+-- ----------------------------------- REVIEWS INFO view
+CREATE OR REPLACE VIEW reviews_info AS
+	SELECT r.title_id AS t_id,
+		   t.title,
+		   r.id AS r_id,
+		   r.body,
+		   CASE (r.is_positive)
+			   WHEN 1 THEN 'positive'
+			   WHEN 0 THEN 'negative'
+			   END AS mood,
+		   review_rate(r.id) AS voted,
+		   u.username
+	  FROM reviews r
+			   JOIN titles t ON r.title_id = t.id
+			   JOIN users u ON r.user_id = u.id
+	 ORDER BY
+		 r.id;
+-- DROP VIEW IF EXISTS reviews_info;
+
+
+-- ---------------------------------------------------------
+-- ----------------------- PROFILE VIEWS -------------------
+-- ---------------------------------------------------------
+
+
+-- ----------------------------------- TITLES PROFILES view
+CREATE OR REPLACE ALGORITHM = TEMPTABLE VIEW t_profiles AS
+	SELECT t.id AS t_id,
+		   t.title,
+		   tt.title_type,
+		   r.avgr AS rating,
+		   r.count AS r_votes,
+		   seen.c AS was_seen,
+		   to_w.c AS on_watchlist,
+		   uli.c AS on_user_lists,
+		   r2.c AS reviewed,
+		   ti.release_date,
+		   ti.rars,
+		   t.original_title,
+		   ti.poster,
+		   ti.tagline,
+		   ti.synopsis
 	  FROM titles AS t
+			   INNER JOIN title_info ti ON t.id = ti.title_id
+			   INNER JOIN title_types tt ON tt.id = ti.title_type_id
 			   LEFT JOIN (SELECT title_id,
-								 round(avg(rating)) AS avg_rating,
-								 round(wavg_rating(title_id)) AS wavg_rating,
+								 round(avg(rating)) AS avgr,
 								 count(rating) AS count
 							FROM rating
 						   GROUP BY title_id
 						 ) AS r ON r.title_id = t.id
+			   LEFT JOIN (SELECT count(is_seen) AS c,
+								 title_id
+							FROM watchlist
+						   WHERE is_seen = 1
+						   GROUP BY title_id
+						 ) seen ON seen.title_id = t.id
+			   LEFT JOIN (SELECT count(is_seen) AS c,
+								 title_id
+							FROM watchlist
+						   WHERE is_seen = 0
+						   GROUP BY title_id
+						 ) to_w ON to_w.title_id = t.id
+			   LEFT JOIN (SELECT count(list_id) AS c,
+								 title_id
+							FROM user_list_items
+						   GROUP BY title_id
+						 ) uli ON t.id = uli.title_id
+			   LEFT JOIN (SELECT count(id) AS c,
+								 title_id
+							FROM reviews
+						   GROUP BY title_id
+						 ) r2 ON t.id = r2.title_id
 	 ORDER BY
 		 t.id;
+-- DROP VIEW IF EXISTS t_profiles;
 
--- TOP 10
 
-SELECT title,
-	   avg_rating -- Top 10 titles by average rating
-  FROM t_ratings
- ORDER BY
-	 avg_rating DESC
- LIMIT 10;
-
-SELECT title,
-	   wavg_rating -- Top 10 titles by weighted average rating
-  FROM t_ratings
- ORDER BY
-	 wavg_rating DESC
- LIMIT 10;
-
--- DROP VIEW IF EXISTS t_ratings;
-
--- ----------------------------------- TITLE PROFILES view
-
-CREATE OR REPLACE ALGORITHM = TEMPTABLE VIEW t_profiles AS
-	SELECT t.id,
-		   t.title,
-		   t.original_title,
-		   tt.title_type,
-		   tr.avg_rating,
-		   tr.wavg_rating,
-		   ti.poster,
-		   ti.tagline,
-		   ti.synopsis,
-		   ti.release_date,
-		   ti.rars
-	  FROM titles AS t
-			   INNER JOIN title_info ti ON t.id = ti.title_id
-			   INNER JOIN title_types tt ON ti.title_type_id = tt.id
-			   LEFT OUTER JOIN t_ratings tr ON tr.title = t.title
+-- ----------------------------------- USERS PROFILES view
+CREATE OR REPLACE VIEW u_profiles AS
+	SELECT u.id AS u_id,
+		   concat_ws(' ', up.first_name, up.last_name) AS name,
+		   u.username,
+		   u.phone_number,
+		   u.email,
+		   up.date_of_birth,
+		   TIMESTAMPDIFF(YEAR, up.date_of_birth, NOW()) AS age,
+		   CASE (up.gender)
+			   WHEN 'm' THEN 'male'
+			   WHEN 'f' THEN 'female'
+			   WHEN 'nb' THEN 'non-binary'
+			   WHEN 'ud' THEN 'undefined'
+			   END AS gender,
+		   CASE (up.is_private)
+			   WHEN 1 THEN 'private'
+			   WHEN 0 THEN 'public'
+			   END AS account,
+		   fu_t.c AS followers,
+		   fu_f.c AS following,
+		   to_w.c AS to_watch,
+		   seen.c AS titles_seen,
+		   ul.lists,
+		   r.c AS reviews,
+		   r2.c AS rated_titles,
+		   r2.avg_rating,
+		   c.country,
+		   up.avatar,
+		   up.about
+	  FROM users u
+			   LEFT JOIN user_profiles up ON u.id = up.user_id
+			   LEFT JOIN countries c ON up.country_id = c.id
+			   LEFT JOIN (SELECT count((id)) AS c, -- Number of followers
+								 target_id
+							FROM follow_user
+						   GROUP BY target_id
+						 ) fu_t ON u.id = fu_t.target_id
+			   LEFT JOIN (SELECT count((id)) AS c, -- Number of users, he/she is following
+								 follower_id
+							FROM follow_user
+						   GROUP BY follower_id
+						 ) fu_f ON u.id = fu_f.follower_id
+			   LEFT JOIN (SELECT user_id,
+								 count(title_id) AS c
+							FROM watchlist
+						   WHERE is_seen = 0
+						   GROUP BY user_id
+						 ) to_w ON u.id = to_w.user_id
+			   LEFT JOIN (SELECT user_id,
+								 count(title_id) AS c
+							FROM watchlist
+						   WHERE is_seen = 1
+						   GROUP BY user_id
+						 ) seen ON u.id = seen.user_id
+			   LEFT JOIN (SELECT count(list_name) AS lists,
+								 user_id
+							FROM user_lists
+						   GROUP BY user_id
+						 ) ul ON u.id = ul.user_id
+			   LEFT JOIN (SELECT count(id) AS c,
+								 user_id
+							FROM reviews
+						   GROUP BY user_id
+						 ) r ON u.id = r.user_id
+			   LEFT JOIN (SELECT count(id) AS c,
+								 round(avg(rating)) AS avg_rating,
+								 user_id
+							FROM rating
+						   GROUP BY user_id
+						 ) r2 ON u.id = r2.user_id
 	 ORDER BY
-		 t.id;
+		 u.id;
+-- DROP VIEW IF EXISTS u_profiles;
 
--- ----------------------------------- TITLE KEYWORDS view
 
-CREATE OR REPLACE VIEW t_keywords AS
-	SELECT t.id, t.title, k.keyword
-	  FROM titles t
-			   JOIN title_keyword tk ON t.id = tk.title_id
-			   JOIN keywords k ON tk.keyword_id = k.id
-	 WHERE keywords(tk.title_id, tk.keyword_id) > 0
-	 GROUP BY
-		 tk.title_id, tk.keyword_id
+-- ----------------------------------- CREATORS PROFILES view
+CREATE OR REPLACE VIEW cr_profiles AS
+	SELECT cr.id AS cr_id,
+		   concat_ws(' ', cr.first_name, cr.last_name) AS name,
+		   TIMESTAMPDIFF(YEAR, cr.date_of_birth, NOW()) AS age,
+		   CASE (cr.gender)
+			   WHEN 'm' THEN 'male'
+			   WHEN 'f' THEN 'female'
+			   WHEN 'nb' THEN 'non-binary'
+			   WHEN 'ud' THEN 'undefined'
+			   END AS gender,
+		   cac.c AS filmography,
+		   cr_r.role,
+		   cr_r.r_count,
+		   cr.date_of_birth,
+		   cn.country
+	  FROM creators cr
+			   JOIN countries cn ON cr.country_id = cn.id
+			   LEFT JOIN (SELECT creator_id,
+								 title_id,
+								 count(title_id) AS c
+							FROM cast_and_crew
+						   GROUP BY creator_id
+						 ) cac ON cac.creator_id = cr.id
+			   LEFT JOIN (SELECT creator_id,
+								 role_id,
+								 r.role AS role,
+								 count(role_id) AS r_count
+							FROM cast_and_crew
+									 JOIN roles r ON cast_and_crew.role_id = r.id
+						   GROUP BY creator_id, role_id
+						   ORDER BY creator_id, r_count DESC
+						 ) AS cr_r ON cr_r.creator_id = cr.id
 	 ORDER BY
-		 tk.title_id;
-
--- ----------------------------------- TITLE GENRES view
-
-CREATE OR REPLACE VIEW t_genres AS
-	SELECT t.id, t.title, g.genre
-	  FROM titles t
-			   JOIN title_genre tg ON t.id = tg.title_id
-			   JOIN genres g ON tg.genre_id = g.id
-	 WHERE genres(tg.title_id, tg.genre_id) > 0
-	 GROUP BY
-		 tg.title_id, tg.genre_id
-	 ORDER BY
-		 tg.title_id;
+		 cr.id;
+-- DROP VIEW IF EXISTS cr_profiles;
